c251cf8f4cbe28c896804e3dd85e23b2181bd206
Jossias Velazquez Peña <jossias.vel@gmail.com>
2017-09-15 17:41:57 -0400

    commint update
    Algunos cambios en algunas clases de la app.

--- ccl.View/Inicio.cs
+++ ccl.View/Inicio.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
 
 namespace ccl.View
 {
-    public partial class Form1 : Form
+    public partial class Inicio : Form
     {
-        public Form1()
+        public Inicio()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
